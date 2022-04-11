749594fd0c8cff7d202d57f2e43f9eac8c70d353
Kayransteelink <33291@ma-web.nl>
2022-04-11 09:12:45 +0200

    first commit

---
+++ getter and setters/getter and setters/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace getter_and_setters
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MyClass myClass = new MyClass();
+            Console.WriteLine(myClass.GetName());
+            myClass.SetName("Jane Doe");
+            Console.WriteLine(myClass.GetName());
+        }
+    }
+
+    internal class MyClass
+    {
+        private string name = "John Doe";
+
+        internal void SetName(string newName)
+        {
+            name = newName;
+        }
+        internal string GetName()
+        {
+            return name;
+        }
+    }
+}
---
+++ scopes/scopes/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace scopes
+{
+    internal class Program
+    {
+
+        public static void call()
+        {
+            {
+                bool x = true;
+                Console.WriteLine(x);
+            }
+        }
+        static void Main(string[] args)
+        {
+            call();
+        }
+    }
+}
